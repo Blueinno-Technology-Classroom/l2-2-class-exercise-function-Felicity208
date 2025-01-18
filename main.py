75f8e6e62b5d443571c7519255d831da5118e429
Felicity <felicity.curreem@gmail.com>
2025-01-18 12:18:44 +0800

    L2 -2

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ Q1:
 
 # TODO: Write your code here
 
+def greet(name):
+    print(f'Hi, {name}!')
+
+greet('Alice')
+greet('Bob')
+print()
 ##################################################
 '''
 Q2:
@@ -12,6 +18,12 @@ Q2:
 
 # TODO: Write your code here
 
+def greet(name1, name2):
+    print(f'Hi,{name1} and {name2}!')
+
+greet('Alex', 'Bob')
+
+print()
 ##################################################
 '''
 Q3:
@@ -19,12 +31,43 @@ Q3:
 
 # TODO: Write your code here
 
+def find_rect_area(width, height):
+    print(f'width ={width}, height = {height}')
+
+find_rect_area(12.3, 45.6)
+print()
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
+
+def lyrics(animal):
+    print(f'Bought me a {animal} and the {animal} pleased me, I fed my {animal} under yonder tree.')
+
+def bought1():
+    print('Cat goes fiddle-i-fee.')
+def bought2():
+    print(' Hen goes chimmy-chuck, chimmy-chuck, Cat goes fiddle-i-fee.')
+def bought3():
+    print('Duck goes quack, quack, Hen goes chimmy-chuck, chimmy-chuck, Cat goes fiddle-i-fee.')
+def bought4():
+    print('Goose goes hissy, hissy, Duck goes quack, quack, Hen goes chimmy-chuck, chimmy-chuck, Cat goes fiddle-i-fee.')
+
+
+
+lyrics('cat')
+bought1()
+lyrics('hen')
+bought2()
+lyrics('duck')
+bought3()
+lyrics('goose')
+bought4()
+    
+    
+
 
 ##################################################
 '''
